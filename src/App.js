6cc42260a9bdfd664a86c74e5506ee2daf1d5141
Solomon Sunmola <sunmolasolomon@gmail.com>
2022-12-21 21:01:16 +0100

    update made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import ExerciseDetails from './Pages/ExerciseDetails';
+import Footer from './components/Footer';
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width:{xl:'1488px'}}} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
