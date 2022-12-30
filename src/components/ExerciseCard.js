da72d1ae1eccc83e6c5e16d608d306374a644398
Solomon Sunmola <sunmolasolomon@gmail.com>
2022-12-30 16:49:44 +0100

    excerside card complete

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack,Typography, Button } from '@mui/material'
+const ExerciseCard = ({exercise}) => {
+  return (
+    <Link 
+        className='exercise-card'
+        to={`/exercise/${exercise.id}`}
+    >
+        <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+        <Stack direction='row'> 
+            <Button
+                sx={{ml:'21px', color:'#fff', background:'#4f7942', fontSize:'14px', borderRadius:'20px', textTransform:'capitalize'}}
+            >
+                {exercise.bodyPart}
+            </Button>
+            <Button
+                sx={{ml:'21px', color:'#fff', background:'#21421e', fontSize:'14px', borderRadius:'20px', textTransform:'capitalize'}}
+            >
+                {exercise.target}
+            </Button>
+        </Stack>
+        <Typography 
+            ml="21px"
+            color='#000'
+            fontWeight='bold'
+            mt='11px'
+            pb='10px'
+            textTransform='capitalize'
+            fontSize='20px'
+        >
+            {exercise.name}
+        </Typography>
+    </Link>
+  )
+}
+
+export default ExerciseCard
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,9 +3,9 @@ import { Pagination } from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
-  console.log(exercises);
   return (
     <Box id='exercises'
       sx={{
@@ -24,7 +24,7 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
         justifyContent='center'
       >
         {exercises.map((exercise, index)=>(
-          <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise}/>
         ))}
       </Stack>
     </Box>
