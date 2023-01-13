5d10d4579137baac5408164a8705f2aee07a097f
Solomon Sunmola <sunmolasolomon@gmail.com>
2023-01-13 10:01:37 +0100

    update

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -5,7 +5,7 @@ import Loader from './Loader'
 
 const SimilarExercises = ({targetMuscleExercises,equipmentExercises}) => {
   return (
-    <Box sx={{mt:{lg:'100px', x:'0'}}}>
+    <Box sx={{mt:{lg:'100px', xs:'0'}}} >
         <Typography variant='h4' mb={5}>Exercise that target the same <span style={{ color: '#1B4D3E', textTransform: 'capitalize' }}>muscle</span>  group</Typography>
         <Stack direction='row' sx={{p:'2', position:'relative'}}>
             {targetMuscleExercises.length ? <HorizontalScrollBar data={targetMuscleExercises} /> : <Loader />}
